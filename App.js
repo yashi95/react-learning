f0940f853ebd4d60ebf320a5b87aa41dae288c7e
yashasvi pandey <yashasvipandey008@gmail.com>
2023-08-30 02:59:36 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,23 +1,68 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const heading = React.createElement("div", { id: "title" }, [
+  React.createElement("h1", {}, "hello H1"),
+  React.createElement("h2", {}, "hello H2"),
+  React.createElement("h3", {}, "hello H3"),
+]);
 
-
-// const inner = document.createElement("h1");
-// inner.innerHTML="Hello World From JavaScript!";
-
-// document.getElementById("mainContainer").appendChild(inner);
-
-const heading = React.createElement(
-    "div",
-    { id: "heading" },
-    React.createElement(
-        "div",
-        { id: "heading2" },
-        "Hello World From React!"
-    )
+const JSXHeading = (
+  <>
+    <div id="title">
+      <h1>hello H1 in JSX</h1>
+      <h2>hello H2 in JSX</h2>
+      <h3>hello H3 in JSX</h3>
+    </div>
+  </>
 );
 
+const FirstComponent = () => {
+  return <h1> This is my component which is Inside another component..!</h1>;
+};
+const CompositionComponent = () => {
+  return <div> This is my First component Inside second component..!</div>;
+};
+
+const SecondComponent = () => (
+  //component composition
+  <div>
+    <CompositionComponent>
+      <FirstComponent />
+    </CompositionComponent>
+    {FirstComponent()}
+    <h1>hello H1 in JSX</h1>
+    <h2>hello H2 in JSX</h2>
+    <h3>hello H3 in JSX</h3>
+  </div>
+);
+
+const Header = () => (
+  <>
+    <header className="header">
+      <h1>Header</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJOzP33YEpqlHMBoLmXcZWDgA8T8haRLh0tQ&usqp=CAU"
+        className="logo"
+        alt="CompanyLogo"
+      />
+      <div className="search-container">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search for products..."
+        />
+        <button className="search-btn">Search</button>
+      </div>
+      <nav>
+        <a href="#home">Home |</a>
+        <a href="#contact">Contact | </a>
+        <a href="#about">About</a>
+      </nav>
+    </header>
+  </>
+);
 const root = ReactDOM.createRoot(document.getElementById("mainContainer"));
-root.render(heading);
+root.render(<Header />);
+
+// root.render(heading);
